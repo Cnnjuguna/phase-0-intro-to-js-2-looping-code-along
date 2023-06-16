8e8ae6393ebc021f988943d06a5e5071aef486de
njugudev <njugudev@pop-os.localdomain>
2023-06-16 21:41:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solutions in this file
+
+
+// console.log(writeCards(names, "birthday"))
+// declare an empty array variable to append the statements printed into it
+//  i want that every statement printed , its then added to an array
+
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event){
+  const newArray = [];
+
+    for(let i = 0; i < names.length; i++){
+
+    newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    
+
+    }
+
+    return newArray;
+}
+
+writeCards(names, "suprise")
+
+// Takes in any positive integer, starting from that number
+// Counts down to zero 0using console.log
+// 
+
+
+
+
+let anyInt = 11
+
+function countDown(anyInt){
+  while (anyInt > -1 ){
+    console.log(anyInt --);
+  }
+}
